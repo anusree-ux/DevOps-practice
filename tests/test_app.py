f035f4b8308d4cbfbc4dfbe94291843cefb18a6f
Ubuntu <ubuntu@ip-172-31-30-190.eu-north-1.compute.internal>
2026-06-15 14:33:04 +0000

    fix failing test expectations

--- tests/test_app.py
+++ tests/test_app.py
@@ -6,7 +6,7 @@ def test_home_route():
     with app.test_client() as client:
         response = client.get("/")
         assert response.status_code == 200
-        assert response.data == b"WRONG RESPONSE"  # This will fail!
+        assert response.data == b"Hello from CI Pipeline!"
 
 
 def test_app_exists():

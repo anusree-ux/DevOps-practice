f1605ae05ca8e8f9cf399d2dcbc0504679fd450d
Ubuntu <ubuntu@ip-172-31-30-190.eu-north-1.compute.internal>
2026-06-15 14:21:24 +0000

    fixed

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,11 +1,13 @@
 from app import app
 
+
 def test_home_route():
     """Test that home route returns correct response"""
     with app.test_client() as client:
-        response = client.get('/')
+        response = client.get("/")
         assert response.status_code == 200
         assert response.data == b"WRONG RESPONSE"  # This will fail!
+
 
 def test_app_exists():
     """Test that Flask app object exists"""
